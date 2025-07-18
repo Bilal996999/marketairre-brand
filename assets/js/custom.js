24084605d69a4ded2ab1a3e357767e3daeb2afb0
Bilal996999 <bilalidrees.bi700@gmail.com>
2025-07-19 04:51:58 +0500

    live v1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -276,6 +276,75 @@ $('.project-slider').slick({
 });
 
 
+$('.testimonial-wrapper').slick({
+  dots: false,
+  arrows: false,
+  infinite: true,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+  speed: 1000,
+  autoplay: true,
+  autoplaySpeed: 3500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  cssEase: 'linear',
+  responsive: [
+    {
+      breakpoint: 1398,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerPadding: "90px",
+      }
+    },
+    {
+      breakpoint: 1198,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerPadding: "80px",
+      }
+    },
+    {
+      breakpoint: 990,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        centerPadding: "70px",
+      }
+    },
+    {
+      breakpoint: 766,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: "60px",
+      }
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: "40px",
+      }
+    },
+    {
+      breakpoint: 450,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: "20px",
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
 
   gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
